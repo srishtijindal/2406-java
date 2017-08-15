80d2fcd3382c73cbf2f3795531789e73092f7c05
srishtijindal <srishti.jindal@my.jcu.edu.au>
2017-08-15 09:59:49 +0530

    Week - 3 work done

---
+++ 3/src/Initials.java
@@ -0,0 +1,27 @@
+public class Initials {
+
+    public static void main(String[] args) {
+        char MYFIRST = 'J', MYMIDDLE = 'M', MYLAST = 'F';
+        DisplayInitials(MYFIRST, MYMIDDLE, MYLAST);
+
+    }
+    public static void DisplayInitials(char first, char middle, char last)
+    {
+
+        System.out.print("The First Initial is ");
+        System.out.println(first);
+        System.out.print("The Middle Initial is ");
+        System.out.println(middle);
+        System.out.print("The Last Initial is ");
+        System.out.println(last);
+
+    }
+}
+
+
+
+
+
+
+
+
