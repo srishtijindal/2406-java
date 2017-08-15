9afa9646e46914f2e7ec4138809bb1269781bbbd
srishtijindal <srishti.jindal@my.jcu.edu.au>
2017-08-15 09:51:05 +0530

    Week - 2 work done

---
+++ src/MoviesQuote.java
@@ -0,0 +1,11 @@
+/**
+ * Created by sony on 2/8/2017.
+ */
+package MoviesQuote.java;
+public class MoviesQuote {
+    public static void main(String[] args)
+    {
+        System.out.println("We can only judge the future by what we have suffered in the past.");
+
+    }
+}
