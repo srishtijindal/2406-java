eead3b7034606f71236ded86cbcb5b6c73c0d436
srishtijindal <srishti.jindal@my.jcu.edu.au>
2017-08-15 09:52:42 +0530

    Week - 2 work done

---
+++ src/MovieQuoteInfo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by sony on 2/8/2017.
+ */
+package MovieQuoteInfo.java;
+public class MovieQuoteInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("We can only judge the future by what we have suffered in the past." );
+        System.out.println("Rise Of An Empire");
+        System.out.println("PJ");
+        System.out.println("2014");
+
+
+
+    }
+}
